1d7c9f6dad6d54a40e99e3c90afd2135f1ad3204
Adrian T <adu@tainic.ro>
2024-11-21 22:18:04 +0200

    add tailwind, jest, ngxs, playwright, material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-material-starter';
+  title = 'angular material starter tailwind jest ngxs playwright';
 }
